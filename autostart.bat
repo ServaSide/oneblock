b0ab2cd88228551e8c8dc9234e2c31eddcf1d2e0
ServaSide <109293088+ServaSide@users.noreply.github.com>
2022-07-14 16:03:47 +0200

    Create autostart.bat

---
+++ autostart.bat
@@ -0,0 +1,9 @@
+@echo off
+
+ECHO x ============================== x
+ECHO |         OneBlock by             |
+ECHO |          ServaSide              |
+ECHO |                                 |
+START oneblock.exe                    
+ECHO |                                 |
+ECHO=============================== x
